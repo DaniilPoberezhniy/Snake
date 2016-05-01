201863380e2857a8b28570eaf29d35cabdaa1350
Daniil Poberezhniy <d.poberzhniy1@gmail.com>
2016-05-01 21:49:10 +0300

    Funtions in the C#

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,8 +10,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 6;
+            int y1 = 8;
+            char symb1 = '*';
+            Draw(x1,y1,symb1);
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char symb)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(symb);
         }
     }
 }
